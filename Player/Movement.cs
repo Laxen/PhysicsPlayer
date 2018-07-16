f71ebac9f46af2c99571d855c9fdabf4aafa9166
Alexander Ganslandt <alex.ganslandt@gmail.com>
2018-07-16 22:47:10 +0200

    initial commit

---
+++ Player/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	public float speed;
+	public float superSpeed;
+
+	public GameObject wall;
+	// public float xOffset;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() {
+		Vector2 v = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		if (Input.GetKey(KeyCode.LeftShift)) 
+			rb.velocity = v * superSpeed;
+		else
+			rb.velocity = v * speed;
+	}
+
+	// void OnDrawGizmos() {
+	// 	Vector3 skrra = wall.transform.position;
+	// 	skrra.x -= xOffset;
+
+	// 	Gizmos.DrawLine(transform.position, skrra);
+	// }
+}
